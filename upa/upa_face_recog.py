130d37c9eedb35b5b3da5a6f96c19c8e8ed217b5
ahsue <abahsue@gmail.com>
2018-05-29 21:01:55 -0700

    phase 2 :D

--- upa/upa_face_recog.py
+++ upa/upa_face_recog.py
@@ -8,34 +8,50 @@ import cv2
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-andrea_image = face_recognition.load_image_file("andrea.jpg")
-andrea_face_encoding = face_recognition.face_encodings(andrea_image)[0]
-ethan_image = face_recognition.load_image_file("ethan.jpg")
-ethan_face_encoding = face_recognition.face_encodings(ethan_image)[0]
-dany_image = face_recognition.load_image_file("dany.jpg")
-dany_face_encoding = face_recognition.face_encodings(dany_image)[0]
+
+import os
+
+imagesKnownEncodings = []
+imagesKnownNames = []
+
+for root, dirs, files in os.walk("./images"):
+    for filename in files:
+        print(filename)
+        imageLoaded = face_recognition.load_image_file("./images/" + filename)
+        imageEncode = face_recognition.face_encodings(imageLoaded)[0]
+        imagesKnownEncodings.append(imageEncode)
+        imagesKnownNames.append(filename[0:len(filename)-4])
+        
+
+#obama_image = face_recognition.load_image_file("images/obama.jpg")
+#obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+#andrea_image = face_recognition.load_image_file("images/andrea.jpg")
+#andrea_face_encoding = face_recognition.face_encodings(andrea_image)[0]
+#ethan_image = face_recognition.load_image_file("images/ethan.jpg")
+#ethan_face_encoding = face_recognition.face_encodings(ethan_image)[0]
+#dany_image = face_recognition.load_image_file("images/dany.jpg")
+#dany_face_encoding = face_recognition.face_encodings(dany_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+#biden_image = face_recognition.load_image_file("images/biden.jpg")
+#biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
-    andrea_face_encoding,
-    ethan_face_encoding,
-    dany_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden" ,
-    "Andrea",
-    "Ethan",
-    "Dany"
-]
+
+#known_face_encodings = [
+  #  obama_face_encoding,
+  #  biden_face_encoding,
+  #  andrea_face_encoding,
+  #  ethan_face_encoding,
+  #  dany_face_encoding
+#]
+#known_face_names = [
+ #   "Barack Obama",
+ #   "Joe Biden" ,
+ #   "Andrea",
+ #   "Ethan",
+ #   "Dany"
+#]
 
 faces_already_detected = []
 
@@ -53,14 +69,14 @@ while True:
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        matches = face_recognition.compare_faces(imagesKnownEncodings, face_encoding)
 
         name = "Unknown"
 
         # If a match was found in known_face_encodings, just use the first one.
         if True in matches:
             first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
+            name = imagesKnownNames[first_match_index]
             personInArray = False
             for x in faces_already_detected:
                 if (name == x):
