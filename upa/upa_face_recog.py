39ef21510a20378f892503c625b3f46463e7c4ba
ahsue <abahsue@gmail.com>
2018-05-30 22:15:52 -0700

    Stage 3 :). This is going faster than expected

--- upa/upa_face_recog.py
+++ upa/upa_face_recog.py
@@ -10,50 +10,26 @@ video_capture = cv2.VideoCapture(0)
 # Load a sample picture and learn how to recognize it.
 
 import os
+import time
 
 imagesKnownEncodings = []
 imagesKnownNames = []
+attendance = []
+start = time.time()
 
+runTime = (int)(input("How long would you like to take attendance for? (in minutes) "))
+runTime *= 60
+
+print("\nExpected students:")
 for root, dirs, files in os.walk("./images"):
     for filename in files:
-        print(filename)
+        print("\t" + filename[0:len(filename)-4].capitalize())
         imageLoaded = face_recognition.load_image_file("./images/" + filename)
         imageEncode = face_recognition.face_encodings(imageLoaded)[0]
         imagesKnownEncodings.append(imageEncode)
-        imagesKnownNames.append(filename[0:len(filename)-4])
-        
-
-#obama_image = face_recognition.load_image_file("images/obama.jpg")
-#obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-#andrea_image = face_recognition.load_image_file("images/andrea.jpg")
-#andrea_face_encoding = face_recognition.face_encodings(andrea_image)[0]
-#ethan_image = face_recognition.load_image_file("images/ethan.jpg")
-#ethan_face_encoding = face_recognition.face_encodings(ethan_image)[0]
-#dany_image = face_recognition.load_image_file("images/dany.jpg")
-#dany_face_encoding = face_recognition.face_encodings(dany_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-#biden_image = face_recognition.load_image_file("images/biden.jpg")
-#biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-# Create arrays of known face encodings and their names
-
-#known_face_encodings = [
-  #  obama_face_encoding,
-  #  biden_face_encoding,
-  #  andrea_face_encoding,
-  #  ethan_face_encoding,
-  #  dany_face_encoding
-#]
-#known_face_names = [
- #   "Barack Obama",
- #   "Joe Biden" ,
- #   "Andrea",
- #   "Ethan",
- #   "Dany"
-#]
-
-faces_already_detected = []
+        imagesKnownNames.append(filename[0:len(filename)-4].capitalize())
+    
+print("\nDetecting faces...")
 
 while True:
     # Grab a single frame of video
@@ -65,7 +41,7 @@ while True:
     # Find all the faces and face enqcodings in the frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
+    
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
@@ -78,13 +54,13 @@ while True:
             first_match_index = matches.index(True)
             name = imagesKnownNames[first_match_index]
             personInArray = False
-            for x in faces_already_detected:
+            for x in attendance:
                 if (name == x):
                     personInArray = True
 
             if (personInArray == False):
-                print ("Person detected: " + name)
-                faces_already_detected.append(name)
+                print ("\t"+ name)
+                attendance.append(name)
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -100,7 +76,17 @@ while True:
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if time.time() > start + runTime : break
 
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
+print("\nAttendance Record")
+print("People present: ")
+for x in attendance:
+    print("\t" + x)
+print("People absent: ")
+for x in imagesKnownNames:
+    if x not in attendance:
+        print("\t" + x)
